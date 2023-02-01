55cde1e94b82302da0e60f5f492288a09954ca99
Md. Abedur Rahman <110812251+D4rk-B0y@users.noreply.github.com>
2023-02-02 02:21:14 +0600

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,19 @@
+import os,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
+    os.system('xdg-open https://www.facebook.com/bap.dakbi')
+import os,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[+] PLIS FOLLOW MY GITHUB')
+    os.system('xdg-open https://github.com/D4rk-B0y')
+
+import Sex
+Sex.menu()
