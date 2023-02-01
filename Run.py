ff9f29fb9d30cba845df88f87cf8f1ac94a1f155
Md. Abedur Rahman <110812251+D4rk-B0y@users.noreply.github.com>
2023-02-02 02:32:45 +0600

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,15 +1,34 @@
-import os, sys, platform 
- try: 
-     import requests 
- except: 
-     os.system('pip install requests') 
- os.system('xdg-open https://github.com/D4rk-B0y') 
+import os, platform, time 
+ print('\n\x1b[1;92m[●] Checking Update.....');time.sleep(0.5) 
+ os.system('git pull') 
+ def Update(): 
+     exit('\033[1;91m[●] Commands On Update Please Wait For Update ❤ ') 
+ def Run(): 
+         bit = platform.architecture()[0] 
+         if bit == '64bit': 
+             print("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools") 
+             print('[●] Follow My Github First') 
+             os.system('xdg-open https://github.com/D4rk-B0y') 
+             import gmx64  
+         elif bit == '32bit': 
+             print("\n\x1b[1;92m[●] Congratulations ! Your Device Support this Tools") 
+             print('[●] Follow My Github First') 
+             os.system('xdg-open https://github.com/D4rk-B0y') 
+             import gmx32 
+         else: 
+             exit('\033[1;31m[●] Connection & Network Error') 
+ Run()
+ 
+     
+ 
+     
+ 
   
- bit = platform.architecture()[0] 
- if bit == '64bit': 
-     import data64 
- elif bit == '32bit': 
-     import data32
+ 
+ 
+     
+ 
+    
 
 
 
